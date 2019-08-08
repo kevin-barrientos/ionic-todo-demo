c27fe6c9f1a0ac2dfc9af0e274bf5ad3cd4397c8
Kevin Barrientos <ing.kevin.b.c@gmail.com>
2019-08-08 00:14:23 -0600

    feat: task form - navigate back to task

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { TasksService } from '../services/tasks.service';
 import { Task } from '../models/task';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'todo-task-form',
@@ -14,12 +15,16 @@ export class TaskFormComponent implements OnInit {
     description: '',
   };
 
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {}
 
   onSaveClicked(task: any): void {
     this.tasksService.add(task);
     this.task = { title: '', dueDate: '', description: '' };
+    this.router.navigate(['tasks']).then(/*ignore*/);
   }
 }
