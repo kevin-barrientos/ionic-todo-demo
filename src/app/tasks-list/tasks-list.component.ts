370c33112d533212dec457e84726e381e4b23cdc
Kevin Barrientos <ing.kevin.b.c@gmail.com>
2019-08-07 19:22:17 -0600

    feat: task list
    Muestra un listado de tareas agregando un arreglo a
    TaskListComponent
    
    Conceptos:
        - String interpolation
        - Pipe
        - @Component decorator

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.css'],
 })
-export class TasksListComponent {}
+export class TasksListComponent {
+  tasks: any[] = [
+    {
+      title: 'Task 1',
+      dueDate: new Date().toISOString(),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+      title: 'Task 2',
+      dueDate: new Date().toISOString(),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+    {
+      title: 'Task 3',
+      dueDate: new Date().toISOString(),
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    },
+  ];
+}
